5f78b686ac886e50a6b01f6b7799f0ce2d245645
amoslanka <amoslanka@gmail.com>
2011-08-29 10:37:17 -0700

    Added parsing of different association types

--- lib/garden/mediators.rb
+++ lib/garden/mediators.rb
@@ -5,23 +5,33 @@ module Garden
       def self.parse_table_name namish
         namish.parameterize.underscore
       end
-      def self.get_instance table_name, id
+      def self.get_instance table, id
 
+        model_name = table.is_a?(ActiveRecord::Reflection::AssociationReflection) ? table.class_name : table.to_s.classify
+        
         # puts "Find #{id} from #{table_name}"
         # puts id.class.to_s
 
         begin
-          clazz = table_name.to_s.classify.constantize
+          clazz = model_name.constantize
           raise "Class #{clazz.to_s} is not an ActiveRecord subclass." unless clazz.new.is_a?(ActiveRecord::Base)
-          instance = clazz.find id.to_s
+          instance = clazz.find(id.to_s)
+          
+          # puts "!! Created clazz #{clazz.to_s} from model_name. No problem. #{instance.class.to_s} #{instance.to_param}"
+          
+          return instance
+        rescue ActiveRecord::RecordNotFound => e
+
+          instance = clazz.find_by_name(id.to_s) if instance.nil? && clazz.respond_to?(:find_by_name)
+          # puts "++Find #{clazz.to_s} by title: #{id}" if instance.nil? && clazz.respond_to?(:find_by_title) 
+          instance = clazz.find_by_title(id.to_s) if instance.nil? && clazz.respond_to?(:find_by_title) 
+          # puts "++#{instance}" if instance
           return instance
         rescue Exception => e
-          puts "Could not find #{id} from table #{table_name}: #{e.message}"
+          puts "Could not find #{id} from table #{model_name}: #{e.message}"
           return nil
         end
       end
-
-      
       
       def initialize namish
         
@@ -45,31 +55,34 @@ module Garden
     
       def parse_headers array
         @headers = array.map { |header| header.to_s.parameterize.underscore }
-        @relationships = []
-        
-        @headers.each do |header|
-          @relationships.push header if @clazz.reflections.keys.include?(header.to_sym)
-        end
-      end
-    
-    
-      def relationships
-        @relationships
+        # @relationships = []
+        # 
+        # @headers.each do |header|
+        #   @relationships.push header if @clazz.reflections.keys.include?(header.to_sym)
+        # end
       end
       
+      # def relationships
+      #   @relationships
+      # end
+      # 
       def create_instance attributes
-        relationships = @relationships
+        # relationships = @relationships
         
-
-        attributes = parse_row_relationships attributes, relationships
+        all_keys = attributes.keys
 
         instance = @clazz.new
+        parse_row_relationships instance, attributes#, relationships
 
-        rejected = {}
-        attributes.each_key do |key|
-          rejected[key] = attributes.delete(key) if !instance.attributes.include?(key.to_s) && !relationships.include?(key.to_s)
-        end
+        # puts "A: #{attributes.keys}"
 
+        # rejected = {}
+        # attributes.each_key do |key|
+        #   rejected[key] = attributes.delete(key) if !instance.attributes.include?(key.to_s)# && !relationships.include?(key.to_s)
+        # end
+
+        
+        attributes.delete_if do |key, value| !instance.attributes.include?(key.to_s) end
         instance.attributes = attributes
 
         # instance.id = rejected[:id] if rejected.key?(:id)
@@ -86,21 +99,53 @@ module Garden
           puts "Invalid instance."
           puts "#{@name}:"
           puts " - Valid attributes: #{attributes.keys}"
-          puts " - Invalid attributes: #{rejected.keys}"
-          puts " - Association attributes: #{relationships}"
+          puts " - Excel sheet keys: #{all_keys}"
+          # puts " - Invalid attributes: #{rejected.keys}"
+          # puts " - Association attributes: #{relationships}"
           puts " - #{instance.errors}"
         end
       end
     
-      def parse_row_relationships hash, relationships
-        relationships.each do |r|
-          # puts "Parsing row relationship: #{hash[r.to_sym]}"
-          instance = get_instance r, hash[r.to_sym]
-          hash[r.to_sym] = instance
+      def parse_row_relationships instance, hash#, relationships
+        @clazz.reflections.each_value do |r|
+          next unless hash.has_key?(r.name)
+          # Remove the value from the hash.
+          value = hash.delete(r.name.to_sym)
+          case r.macro
+          when :has_many 
+            parse_has_many(instance, r, value)
+          when :belongs_to 
+            parse_belongs_to(instance, r, value)
+          end
         end
-        hash
+        
+        # relationships.each do |r|
+        #   # puts "Parsing row relationship: #{hash[r.to_sym]}"
+        #   instance = get_instance r, hash[r.to_sym]
+        #   hash[r.to_sym] = instance
+        # end
+        # hash
       end
-
+      
+      
+      def parse_has_many instance, r, value
+        # puts " // Parsing #{r.name}, #{value}"
+        ids = value.split(/\s*,\s*/)
+        ids.each do |id|
+          related_instance = Table.get_instance r.class_name, id
+          # puts ">> Creating a has-many relationship. #{id}, #{related_instance}"
+          unless related_instance.nil?
+            instance.send(r.name.to_sym) << related_instance
+          end
+        end
+      end
+    
+      def parse_belongs_to instance, r, value
+        ri = Table.get_instance(r, value)
+        instance.attributes = { r.name.to_sym => ri }
+        
+        # puts "Parsing Belongs_To. #{instance.class} #{instance.to_param} --- #{r.name.to_sym} --- #{ri.class} #{ri.to_param}"
+      end
     
     end
   end
