45e37ea98f6fe9f27cd7fee111c40711485d6848
amoslanka <amoslanka@gmail.com>
2011-09-29 15:00:50 -0700

    Better ActiveRecord class detection. Now allows for namespaced classes.

--- lib/garden/mediators.rb
+++ lib/garden/mediators.rb
@@ -2,21 +2,49 @@ module Garden
   module Mediators
     class Table
       
-      def self.parse_table_name namish
-        namish.parameterize.underscore
+      # def self.parse_table_name namish
+      #   namish.parameterize.underscore
+      # end
+      
+      def self.find_model_class namish
+        clazz = nil
+        namish = namish.singularize
+        
+        begin
+          # try the format as it should be. Camelcase, and with :: or / for modules
+          # Module::Classname
+          clazz = namish.underscore.classify.constantize
+        rescue
+          begin
+            # try the format "Class Name" by turning spaced words into camelcase
+            clazz = namish.parameterize.underscore.classify.constantize
+          rescue
+            begin
+              # try the format "Class Name" by turning spaced words into module separators
+              clazz = namish.gsub(/ /, "/").underscore.classify.constantize
+            rescue
+              puts "Nope"
+            end
+          end
+        end
+        clazz
       end
       
       def initialize namish
         
-        @name = Table.parse_table_name namish
+        @clazz = Table.find_model_class namish
+        
+        if @clazz.nil? || !@clazz.new.is_a?(ActiveRecord::Base)
+          raise "Class '#{namish}' could not be parsed to an ActiveRecord subclass."
+        end
         
         # Get the ActiveRecord model from the tablename.
-        begin
-          @clazz = @name.classify.constantize
-          raise "Class #{@clazz.to_s} is not an ActiveRecord subclass." unless @clazz.new.is_a?(ActiveRecord::Base)
-        rescue Exception => e
-          puts " ** Could not derive ActiveRecord model from the provided name: #{namish}. Exception: #{e.message}"
-        end
+        # begin
+        #   @clazz = @name.classify.constantize
+        #   raise "Class #{@clazz.to_s} is not an ActiveRecord subclass." unless @clazz.new.is_a?(ActiveRecord::Base)
+        # rescue Exception => e
+        #   puts " ** Could not derive ActiveRecord model from the provided name: #{namish}. Exception: #{e.message}"
+        # end
         
         @instance = @clazz.new
         
@@ -43,6 +71,15 @@ module Garden
         Instance.new @clazz, attributes
       end
 
+
+      def class_exists?(name)
+        begin
+          true if Kernel.const_get(name)
+        rescue NameError
+          false
+        end
+      end
+      
     end
   end
 end
